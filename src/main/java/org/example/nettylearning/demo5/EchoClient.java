8063b60457e90e9d278cdddcf230de921afbd989
ljzzkkkss <793958447@qq.com>
2021-03-15 16:30:26 +0800

    netty learning for EchoServer by DelimiterBasedFrameDecoder.

---
+++ src/main/java/org/example/nettylearning/demo5/EchoClient.java
@@ -0,0 +1,61 @@
+package org.example.nettylearning.demo5;
+
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.DelimiterBasedFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
+
+import java.nio.charset.StandardCharsets;
+
+public class EchoClient {
+    public static void main(String[] args) {
+        int port = 8880;
+        if(args != null && args.length > 0){
+            try {
+                port = Integer.parseInt(args[0]);
+            }catch (NumberFormatException exception){
+                exception.printStackTrace();
+                //采用默认值
+            }
+        }
+
+        new EchoClient().connect(port,"127.0.0.1");
+    }
+
+    private void connect(int port, String host) {
+        EventLoopGroup group = new NioEventLoopGroup();
+        try{
+            Bootstrap bootStrap = new Bootstrap();
+            bootStrap.group(group)
+                    .channel(NioSocketChannel.class)
+                    .option(ChannelOption.TCP_NODELAY,true)
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) {
+                            ByteBuf byteBuf = Unpooled.copiedBuffer("$_".getBytes(StandardCharsets.UTF_8));
+                            ch.pipeline()
+                                    .addLast(new DelimiterBasedFrameDecoder(1024,byteBuf))
+                                    .addLast(new StringDecoder())
+                                    .addLast(new EchoClientHandler());
+                        }
+                    });
+            //发起异步连接操作
+            ChannelFuture future = bootStrap.connect(host,port).sync();
+            //等待客户端链路关闭
+            future.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            group.shutdownGracefully();
+        }
+    }
+}
---
+++ src/main/java/org/example/nettylearning/demo5/EchoServer.java
@@ -0,0 +1,67 @@
+package org.example.nettylearning.demo5;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.DelimiterBasedFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
+import java.nio.charset.StandardCharsets;
+
+public class EchoServer {
+    public static void main(String[] args) {
+        int port = 8880;
+        if(args != null && args.length > 0){
+            try {
+                port = Integer.parseInt(args[0]);
+            }catch (NumberFormatException exception){
+                exception.printStackTrace();
+                //采用默认值
+            }
+        }
+
+        new EchoServer().bind(port);
+    }
+
+    public void bind(int port){
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        ServerBootstrap bootstrap = new ServerBootstrap();
+        try {
+            bootstrap.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG, 100)
+                    .handler(new LoggingHandler(LogLevel.INFO))
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            ByteBuf delimiter = Unpooled.copiedBuffer("$_".getBytes(StandardCharsets.UTF_8));
+                            socketChannel.pipeline()
+                                    .addLast(new DelimiterBasedFrameDecoder(1024, delimiter));
+                            socketChannel.pipeline().addLast(new StringDecoder());
+                            socketChannel.pipeline().addLast(new EchoServerHandler());
+                        }
+                    });
+            //绑定端口，同步等待成功
+            ChannelFuture future = bootstrap.bind(port).sync();
+
+            //等待服务端监听端口关闭
+            future.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
